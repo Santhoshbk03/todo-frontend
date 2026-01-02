d4d34b2b7e060edc27c5005a1603dbb8e871e1e3
Santhosh Babu <santhosh.b@maxbytetech.com>
2026-01-02 12:30:47 +0530

    protect routess

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route index path="/login" element={<LoginPage />} />
 
         <Route
           path="/"
